5350c21a0b2fab494b6be954785ee8455393f32a
Dina Li <dinali@ers.usda.gov>
2013-02-14 14:04:57 -0500

    Initial Commit

---
+++ IndicatorsTests/IndicatorsTests.h
@@ -0,0 +1,13 @@
+//
+//  IndicatorsTests.h
+//  IndicatorsTests
+//
+//  Created by Dina Li on 2/14/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IndicatorsTests : SenTestCase
+
+@end
---
+++ IndicatorsTests/IndicatorsTests.m
@@ -0,0 +1,32 @@
+//
+//  IndicatorsTests.m
+//  IndicatorsTests
+//
+//  Created by Dina Li on 2/14/13.
+//  Copyright (c) 2013 USDA ERS. All rights reserved.
+//
+
+#import "IndicatorsTests.h"
+
+@implementation IndicatorsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IndicatorsTests");
+}
+
+@end
